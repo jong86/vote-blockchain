8b81da8de7224c943831a81d74503d6db72049a7
Jon Gaspar <jon_gaspar@hotmail.com>
2018-04-08 21:19:57 -0700

    Changed out MetaCoin contract for Voting contract

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -2,33 +2,47 @@ pragma solidity ^0.4.17;
 
 import "./ConvertLib.sol";
 
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+contract Voting {
+	/* mapping field below is equivalent to an associative array or hash.
+	The key of the mapping is candidate name stored as type bytes32 and value is
+	an unsigned integer to store the vote count
+	*/
 
-contract MetaCoin {
-	mapping (address => uint) balances;
+    mapping (bytes32 => uint8) public votesReceived;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	/* Solidity doesn't let you pass in an array of stirngs in the constructor (yet).
+    We will use an array of bytes32 instead to store the list of candidates
+    */
 
-	function MetaCoin() public {
-		balances[tx.origin] = 10000;
-	}
+    bytes32[] public candidateList;
 
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
-		return true;
-	}
+    /* This is the constructor which will be called once when you
+    deploy the contract to the blockhain. When we deploy the contract,
+    we will pass an array of candidates who will be contesting in the election
+    */
+    function Voting(bytes32[] candidateNames) public {
+        candidateList = candidateNames;
+    }
 
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
+    // This function returns the total votes a candidate has received so far
+    function totalVotesFor(bytes32 candidate) view public returns (uint8) {
+        require(validCandidate(candidate));
+        return votesReceived[candidate];
+    }
 
-	function getBalance(address addr) public view returns(uint) {
-		return balances[addr];
-	}
+    // This function increments the vote count for the specified candidate.
+    // This is equivalent to casting a vote
+    function voteForCandidate(bytes32 candidate) public {
+        require(validCandidate(candidate));
+        votesReceived[candidate] += 1;
+    }
+
+    function validCandidate(bytes32 candidate) view public returns (bool) {
+        for(uint i = 0; i < candidateList.length; i++) {
+            if (candidateList[i] == candidate) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
